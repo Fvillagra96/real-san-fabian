360c0e420b726623ade99ce24af2c78d23a10531
Fvillagra96 <fvillagra.mendez@gmail.com>
2026-09-07 14:24:34 -0300

    Configuración de Firebase1

--- src/app/finanzas/page.js
+++ src/app/finanzas/page.js
@@ -1,8 +1,8 @@
 "use client";
 import { useState, useEffect } from 'react';
 // IMPORTANTE: Asegúrate de que esta ruta apunte a donde creaste tu firebase.js
-import { db, storage } from '../firebase'; 
-import { collection, addDoc, getDocs, updateDoc, deleteDoc, doc } from 'real-san-fabian/lib/firebase,js';
+import { db, storage } from '../../../lib/firebase';
+import { collection, addDoc, getDocs, updateDoc, deleteDoc, doc } from 'firebase/firestore';
 import { ref, uploadBytes, getDownloadURL } from 'firebase/storage';
 
 export default function FinanzasPage() {
