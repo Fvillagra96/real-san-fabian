9515570d1259917092c1f365cfd8a3203738f30b
Fvillagra96 <fvillagra.mendez@gmail.com>
2026-09-07 14:42:33 -0300

    Forzar subida de firebase.js a la carpeta app

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-black p-24">
       <h1 className="text-6xl font-bold text-purple-600 text-center">
-        Real San Fabián
+        Real San Fabiánz
       </h1>
       <p className="mt-4 text-xl text-gray-300 text-center">
         Plataforma de Administración Deportiva
